8360a5323c6137294543eb9fed799e8be642fa95
AshHasib <ashhasib54@gmail.com>
2019-05-07 20:38:49 +0600

    TabLayout setup complete including fragments

--- app/src/main/java/com/example/hasib/slave/MainActivity.java
+++ app/src/main/java/com/example/hasib/slave/MainActivity.java
@@ -1,20 +1,67 @@
 package com.example.hasib.slave;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
+import com.example.hasib.slave.adapters.ViewPagerAdapter;
+import com.example.hasib.slave.fragments.HomeFragment;
+import com.example.hasib.slave.fragments.NewsFragment;
+import com.example.hasib.slave.fragments.TasksFragment;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+    private ActionBar actionBar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //FirebaseApp.initializeApp(this);
-        FirebaseDatabase.getInstance().getReference().setValue("Hello");
 
+        initToolbar();
+        initTabLayout();
 
     }
+
+
+
+
+
+
+    private void initToolbar() {
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        actionBar=getSupportActionBar();
+        actionBar.setTitle("Slave");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+    }
+
+
+    private void initTabLayout(){
+        tabLayout=findViewById(R.id.tabLayout);
+        viewPager=findViewById(R.id.viewPager);
+        ViewPagerAdapter adapter=new ViewPagerAdapter(getSupportFragmentManager());
+
+        adapter.addFragments("Home",new HomeFragment());
+        adapter.addFragments("Tasks",new TasksFragment());
+        adapter.addFragments("News",new NewsFragment());
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
 }
---
+++ app/src/main/java/com/example/hasib/slave/adapters/ViewPagerAdapter.java
@@ -0,0 +1,44 @@
+package com.example.hasib.slave.adapters;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment> fragmentList=new ArrayList<>();
+    private final List<String> fragmentTitles=new ArrayList<>();
+
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return fragmentList.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentTitles.get(position);
+    }
+
+
+    public void addFragments(String title,Fragment fragment) {
+        fragmentList.add(fragment);
+        fragmentTitles.add(title);
+    }
+
+
+}
